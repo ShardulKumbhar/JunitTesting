8950810311afd64a8e519c0e685e155fe3f733c6
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-15 14:09:56 +0530

    added sample emails validation problem

--- src/main/java/sample/Validation1.java
+++ src/main/java/sample/Validation1.java
@@ -1,11 +1,71 @@
 package sample;
 
-public class Validation1 {
+import java.util.regex.Pattern;
 
-	/*
-	 * 1.creating method to check email pattern
+public class Validation1 {
+	/**
+	 * PROCEDURE
+	 * ==============================================
+	 * 1.created varous patterns to match
+	 * 2.created method to check first name
+	 * 3.created method to check last name
+	 * 4. created method to check email
+	 * 5.created method to check phone number
+	 * 6. created method to check password
+	 * ================================================
 	 */
-	public static boolean email(String mail) {
-		return mail.matches("^[a-zA-Z0-9]+([._+-][0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$");
-	}
+	
+	/*
+	 * 1.created varous patterns to match
+	 */
+    private static final String namePattern = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String emailPattern = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+    private static final String phonePattern = "^[0-9]{2}[ ]?[6-9]{1}[0-9]{9}$";
+    private static final String passwordPattern = "^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\\W).*$";
+
+    /*
+     * 2.created method to check first name
+     */
+    public boolean validateFirstName(String fname){
+        Pattern pattern = Pattern.compile(namePattern);
+ 
+        return pattern.matches(namePattern,fname);
+       
+    }
+
+    /*
+     * 3.created method to check last name
+     */
+    public boolean validateLastName(String lName) {
+        Pattern pattern = Pattern.compile(namePattern);
+        
+        return pattern.matches(namePattern,lName);
+        
+    }
+    /*
+     *4. created method to check email
+     */
+    public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(emailPattern);
+        return pattern.matches(emailPattern,email);
+    }
+
+    /*
+     * 5.created method to check phone number
+     */
+    public boolean validatePhoneNumber(String phNumber) {
+    	
+        Pattern pattern = Pattern.compile(phonePattern);
+        return pattern.matches(phonePattern,phNumber);
+    }
+
+    /*
+     *6. created method to check password
+     */
+    public boolean validatePassword(String password) {
+        Pattern pattern = Pattern.compile(passwordPattern);
+        return pattern.matches(passwordPattern,password);
+    
+    }
 }
+
--- src/test/java/sample/UnitTesting1.java
+++ src/test/java/sample/UnitTesting1.java
@@ -4,60 +4,111 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import userregistration.UserRegistration;
+
 
 
 public class UnitTesting1 { 
-	
 	/**
-	 * steps
-	 * =================================
-	 * 1. taking vaid email sample
-	 * 2. taking unvaid email sample
-	 * 3.testing valid emails
-	 * 4.testing unvalid emails
-	 * ==================================
-	 */
-	
-	
-	/*
-	 * 1. taking vaid email sample
+	 * STEPS
+	 * ==============================================
+	 * 1.created object of class
+	 * 2.takeng sample email to test
+	 * 3.common steps to tests
+	 * 4. testing all valid email by applying loop
+	 * 5.testing all unvalidemail by applying loop
+	 * ================================================
 	 */
 
+	
+            /*
+             * 1.created object of class
+             */
+	Validation1 userReg = new Validation1();
+
+	
+	/*
+	 * 2.takeng sample email to test
+	 * storing in String valid / unvalid emails
+	 */
+	
 	public String[] validemail = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
 			"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com" };
 
-	/*
-	 * 2. taking unvaid email sample
-	 */
 	public String[] unvalidemail = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
 			"..abc@abc.com", ".abc()@gmail.com", "abc@%.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a",
 			"abc@gmail.com.aa.au" };
 
+
+
+
 	/*
-	 * 3.testing valid emails
+	 * 3.common steps to tests
+	 * ====================================================
+	 * testing first name
+	 * taking boolean output
+	 * calling method to check pattern and matcher
+	 * comparing output with assertequals 
+	 * ====================================================
 	 */
+	@Test
+	public void firstName() {
+		boolean result = userReg.validateFirstName("Shardul");
+		assertEquals(true, result);
+	}
 
 	@Test
-	public void test1() {
-		for (int i = 0; i < validemail.length; i++) {
-			String checker1 = validemail[i];
+	public void lastName() {
+		boolean result = userReg.validateLastName("Kumbhar");
+		assertEquals(true, result);
+	}
 
-			Boolean email = Validation1.email(checker1);
+	@Test
+	public void phoneNumber() {
+		boolean result = userReg.validatePhoneNumber("91 7709125030");
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void password() {
+		boolean result = userReg.validatePassword("Kumbhar@123");
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void email() {
+		boolean result = userReg.validateEmail("yahhoo@123.com");
+		assertEquals(true, result);
+	}
+	
+	
+	/*
+	 *4. testing all valid email by applying loop
+	 */
+	@Test
+	public void validEmailTest() {
+		for (String sample : validemail) {
+
+			String checker1 = sample;
+
+			Boolean email = userReg.validateEmail(checker1);
 			assertEquals(true, email);
 		}
+
 	}
 
 	/*
-	 * 4.testing unvalid emails
+	 * 5.testing all unvalidemail by applying loop
 	 */
-
 	@Test
-	public void test2() {
-		for (int i = 0; i < unvalidemail.length; i++) {
-			String checker2 = unvalidemail[i];
+	public void unvalidEmailTest() {
 
-			Boolean email = Validation1.email(checker2);
+		for (String sample : unvalidemail) {
+			String checker1 = sample;
+
+			Boolean email = userReg.validateEmail(checker1);
 			assertEquals(false, email);
 		}
 	}
+
 }
